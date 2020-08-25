0eff76f4a6760104cb286f04b321fee7e7f74ab9
lyons <gy_lyons@163.com>
2020-08-26 00:05:29 +0800

    得分刷新

--- assets/script/game.js
+++ assets/script/game.js
@@ -4,7 +4,8 @@ cc.Class({
     properties: {
         ballNode: cc.Node,
         blockPrefab: cc.Prefab,
-        blockAreaNode: cc.Node
+        blockAreaNode: cc.Node,
+        scoreLabel: cc.Label
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -12,9 +13,9 @@ cc.Class({
     onLoad () {
         this.initPhysics()
         this.initBlock()
-
+        this.score = 0
         this.node.on('touchstart', this.boost, this)
-        this.gameStart = 0
+        this.gameStart = false
     },
 
     onDestroy () {
@@ -29,11 +30,24 @@ cc.Class({
                 blockNode.x += speed
 
                 if (blockNode.x < - cc.winSize.width / 2 - blockNode.width / 2) {
+                    this.incrScore(1)
                     blockNode.x = this.getLastBlockPosX() + 200
                 }
             }
         }
+        if (this.ballNode.y < -cc.winSize.height / 2) {
+            console.log('gameover')
+            cc.director.loadScene('game')
+        }
     },
+
+    // 刷新得分
+    incrScore (incr) {
+        this.score += incr
+        this.scoreLabel.string = this.score
+    },
+
+    // 获取最后一块跳板位置
     getLastBlockPosX () {
         let posX = 0
         for (let blockNode of this.blockNodeArr) {
@@ -71,7 +85,7 @@ cc.Class({
         if (this.ballNode.getComponent('ball').initVel) {
             let rigidBody = this.ballNode.getComponent(cc.RigidBody)
             rigidBody.linearVelocity = cc.v2(0, -1600)
-            this.gameStart = 1
+            this.gameStart = true
         }
     }
 });
