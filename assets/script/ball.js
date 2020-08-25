51c6fe6baf44de9c3371e6907f094419ad2eebcd
lyons <gy_lyons@163.com>
2020-08-25 23:58:07 +0800

    自动生成板块

--- assets/script/ball.js
+++ assets/script/ball.js
@@ -4,15 +4,15 @@ cc.Class({
     properties: {
     },
     onLoad () {
-        this.initVal = 0
+        this.initVel = 0
     },
 
     onBeginContact (contact, selfCollider, otherCollider) {
         let rigidBody = selfCollider.node.getComponent(cc.RigidBody)
-        if (!this.initVal) {
-            this.initVal = rigidBody.linearVelocity.y // 记录初始速度
+        if (!this.initVel) {
+            this.initVel = rigidBody.linearVelocity.y // 记录初始速度
         } else {
-            rigidBody.linearVelocity = cc.v2(0, this.initVal) // 重置初始速度
+            rigidBody.linearVelocity = cc.v2(0, this.initVel) // 重置初始速度
         }
     }
 });
--- assets/script/game.js
+++ assets/script/game.js
@@ -21,18 +21,40 @@ cc.Class({
         this.node.off('touchstart', this.boost, this)
     },
 
+    update (dt) {
+        if (this.gameStart) {
+            let speed = -350 * dt
+
+            for (let blockNode of this.blockNodeArr) {
+                blockNode.x += speed
+
+                if (blockNode.x < - cc.winSize.width / 2 - blockNode.width / 2) {
+                    blockNode.x = this.getLastBlockPosX() + 200
+                }
+            }
+        }
+    },
+    getLastBlockPosX () {
+        let posX = 0
+        for (let blockNode of this.blockNodeArr) {
+            if (blockNode.x > posX) {
+                posX = blockNode.x
+            }
+        }
+        return posX
+    },
     // 初始化跳板
     initBlock () {
-        this.lastBlocKPos = this.ballNode.x  // 最后一个方块的x轴
+        this.lastBlocKPosX = this.ballNode.x  // 最后一个方块的x轴
         this.blockNodeArr = []
         for (let i = 0; i < 10; i++) {
             let blockNode = cc.instantiate(this.blockPrefab)
-            blockNode.x = this.lastBlocKPos
+            blockNode.x = this.lastBlocKPosX
             blockNode.y = 0
             this.blockAreaNode.addChild(blockNode)
             this.blockNodeArr.push(blockNode)
 
-            this.lastBlocKPos += 200
+            this.lastBlocKPosX += 200
         }
     },
 
@@ -45,8 +67,11 @@ cc.Class({
 
     // 加速
     boost (e) {
-        let rigidBody = this.ballNode.getComponent(cc.RigidBody)
-        rigidBody.linearVelocity = cc.v2(0, -1600)
-        console.log('加速')
+        console.log(123)
+        if (this.ballNode.getComponent('ball').initVel) {
+            let rigidBody = this.ballNode.getComponent(cc.RigidBody)
+            rigidBody.linearVelocity = cc.v2(0, -1600)
+            this.gameStart = 1
+        }
     }
 });
